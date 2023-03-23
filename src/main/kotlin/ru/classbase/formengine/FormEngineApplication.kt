228765b12ceff2b72b4dc703454bfa4643865803
pzaharoff <pzaharoff@gmail.com>
2023-03-23 11:38:23 +0500

    Initial Commit

---
+++ src/main/kotlin/ru/classbase/formengine/FormEngineApplication.kt
@@ -0,0 +1,11 @@
+package ru.classbase.formengine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FormEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<FormEngineApplication>(*args)
+}
---
+++ src/test/kotlin/ru/classbase/formengine/FormEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.classbase.formengine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FormEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
